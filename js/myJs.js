af441608636fc5b017cec0adfaab57b6424a4fd8
hola <101407517+woaixiatian18@users.noreply.github.com>
2022-04-01 08:19:50 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -107,7 +107,7 @@ $(document).ready(function () {
 
   // show popup
   $("#yes").click(function () {
-    var audio = new Audio("sound/tick.mp3");
+    var audio = new Audio("audido.m4a");
     audio.play();
     Swal.fire({
       title: textConfig.text7,
